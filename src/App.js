62cf3ab313b94bbd079b6bc5949c90041a870d6a
jyothiswaroop07 <vv5382@srmist.edu.in>
2024-10-30 22:08:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <LoveLetter />
-      <h1 className='instruction'>Jus click the envelope and zoom out❤️</h1>
+      
     </div>
   );
 }
