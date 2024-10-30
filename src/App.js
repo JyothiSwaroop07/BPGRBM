33f5a41f8f425be761006ff9ffe97984e2012455
jyothiswaroop07 <vv5382@srmist.edu.in>
2024-10-30 21:58:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import LoveLetter from './components/LoveLetter/LoveLetter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LoveLetter />
+      <h1 className='instruction'>Jus click the envelope and zoom out❤️</h1>
+    </div>
+  );
+}
+
+export default App;
