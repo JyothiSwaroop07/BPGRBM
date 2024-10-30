e2593f8e65bf5b6874bd43e0485a868482120877
jyothiswaroop07 <vv5382@srmist.edu.in>
2024-10-30 22:10:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h3 className='instruction'>Click on the envelope please...</h3>
       <LoveLetter />
       
     </div>
